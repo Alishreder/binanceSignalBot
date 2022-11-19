cbbe70cd4b24013520b7f8c45432b25c9308ebb5
Alishreder <aleksboyko12@gmail.com>
2022-11-19 17:21:56 +0200

    load .env

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -9,11 +9,16 @@ import (
 	"github.com/Alishreder/binanceSignalBot/pkg/repository/postgresDB"
 	"github.com/Alishreder/binanceSignalBot/pkg/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Panic(err)
+	}
 
 	telegramToken := os.Getenv("TELEGRAM_TOKEN")
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.4.0
 	gorm.io/driver/postgres v1.3.8
 	gorm.io/gorm v1.23.8
 )
